8d450a2664e9a499064cb5f0168d61b7861325b2
Gonzalo Lucero <glucero@adecoagro.com>
2022-06-23 22:34:03 -0300

    holasi4

--- client/src/Componentes/Inicio.jsx
+++ client/src/Componentes/Inicio.jsx
@@ -27,8 +27,8 @@ export default function Inicio() {
     <div className="div1">
       <div className="div">
         <h1>LA CALLE ANDA DICIENDO</h1>
-        <h2>"La universidad está atravesada por problemáticas del mundo contemporáneo. <br/>Sumate a evidenciar públicamente las problemáticas que te afectan</h2>
-        <input type="input" className="input" placeholder="Frase..." value={nombre} />
+        <h2>"La universidad está atravesada por problemáticas del mundo contemporáneo. <br/>Sumate a evidenciar públicamente las problemáticas que te afectan"</h2>
+        <input type="input" className="input" placeholder="Frase..." value={nombre} onChange={e.target.value} />
         <button type="submit" className="button" onClick={() => buscar()}> Enviar </button>
       </div>
       </div>
